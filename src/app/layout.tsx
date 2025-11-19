612ffb24411a2431b28cc7862844efaf0dbd8b1f
Abhishek Singh <aahabhisheksingh@gmail.com>
2025-11-19 18:35:33 +0530

    ads

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2968140045653690"
+     crossOrigin="anonymous"></Script>
         {children}
       </body>
     </html>
